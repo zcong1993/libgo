189fd827db515fb41c59d28748dcdc44a4db0d63
zcong1993 <1005997919@qq.com>
2018-12-13 22:24:42 +0800

    feat(gin): add pagination parser

---
+++ gin/pagination.go
@@ -0,0 +1,33 @@
+package ginerr
+
+import (
+	"github.com/gin-gonic/gin"
+	"strconv"
+)
+
+func parsePaginationArg(c *gin.Context, key string, defaultValue int) int {
+	valueStr, ok := c.GetQuery(key)
+	if !ok {
+		return defaultValue
+	}
+
+	value, err := strconv.Atoi(valueStr)
+
+	if err != nil {
+		return defaultValue
+	}
+
+	if value <= 0 {
+		return defaultValue
+	}
+
+	return value
+}
+
+type OffsetLimitPaginator struct {
+	DefaultNumPerPage int
+}
+
+func (op *OffsetLimitPaginator) ParsePagination(c *gin.Context) (limit, offset int) {
+	return parsePaginationArg(c, "limit", op.DefaultNumPerPage), parsePaginationArg(c, "offset", 0)
+}
