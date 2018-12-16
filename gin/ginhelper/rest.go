20cd9e318a12b1fe1042d58646c258ba5b6325d6
zcong1993 <1005997919@qq.com>
2018-12-17 00:59:41 +0800

    update rest

--- gin/ginhelper/rest.go
+++ gin/ginhelper/rest.go
@@ -6,6 +6,16 @@ import (
 	"net/http"
 )
 
+const (
+	List = iota
+	Create
+	Retrieve
+	Update
+	Destroy
+)
+
+var ReadOnly = []uint{List, Retrieve}
+
 type IRest interface {
 	List(ctx *gin.Context)
 	Create(ctx *gin.Context)
@@ -36,33 +46,70 @@ func (r *Rest) Destroy(ctx *gin.Context, id string) {
 	ctx.Status(http.StatusMethodNotAllowed)
 }
 
-func BindRouter(r gin.IRoutes, prefix string, rest IRest) {
+func BindRouter(r gin.IRoutes, prefix string, rest IRest, methods ...uint) {
 	withID := fmt.Sprintf("%s/:id", prefix)
-	// list
-	r.GET(prefix, func(c *gin.Context) {
-		rest.List(c)
-	})
+	if len(methods) == 0 {
+		// list
+		r.GET(prefix, func(c *gin.Context) {
+			rest.List(c)
+		})
 
-	// post
-	r.POST(prefix, func(c *gin.Context) {
-		rest.Create(c)
-	})
+		// post
+		r.POST(prefix, func(c *gin.Context) {
+			rest.Create(c)
+		})
 
-	// get one
-	r.GET(withID, func(c *gin.Context) {
-		id := c.Param("id")
-		rest.Retrieve(c, id)
-	})
+		// get one
+		r.GET(withID, func(c *gin.Context) {
+			id := c.Param("id")
+			rest.Retrieve(c, id)
+		})
 
-	// delete one
-	r.DELETE(withID, func(c *gin.Context) {
-		id := c.Param("id")
-		rest.Destroy(c, id)
-	})
+		// delete one
+		r.DELETE(withID, func(c *gin.Context) {
+			id := c.Param("id")
+			rest.Destroy(c, id)
+		})
 
-	// update one
-	r.PATCH(withID, func(c *gin.Context) {
-		id := c.Param("id")
-		rest.Update(c, id)
-	})
+		// update one
+		r.PATCH(withID, func(c *gin.Context) {
+			id := c.Param("id")
+			rest.Update(c, id)
+		})
+
+		return
+	}
+
+	for _, v := range methods {
+		switch v {
+		case List:
+			// list
+			r.GET(prefix, func(c *gin.Context) {
+				rest.List(c)
+			})
+		case Retrieve:
+			// get one
+			r.GET(withID, func(c *gin.Context) {
+				id := c.Param("id")
+				rest.Retrieve(c, id)
+			})
+		case Create:
+			// post
+			r.POST(prefix, func(c *gin.Context) {
+				rest.Create(c)
+			})
+		case Update:
+			// update one
+			r.PATCH(withID, func(c *gin.Context) {
+				id := c.Param("id")
+				rest.Update(c, id)
+			})
+		case Destroy:
+			// delete one
+			r.DELETE(withID, func(c *gin.Context) {
+				id := c.Param("id")
+				rest.Destroy(c, id)
+			})
+		}
+	}
 }
