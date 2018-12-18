e70517f6cccae96dee15a289fb441a0bb23daea3
zcong1993 <1005997919@qq.com>
2018-12-18 23:14:17 +0800

    update rest

--- gin/ginhelper/pagination.go
+++ gin/ginhelper/pagination.go
@@ -2,8 +2,13 @@ package ginhelper
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	"github.com/zcong1993/libgo/utils"
+	"net/http"
 	"strconv"
 )
+
+const HEADER_TOTAL_COUNT = "X-TOTAL-COUNT"
 
 func parsePaginationArg(c *gin.Context, key string, defaultValue int) int {
 	valueStr, ok := c.GetQuery(key)
@@ -37,3 +42,26 @@ func (op *OffsetLimitPaginator) ParsePagination(c *gin.Context) (limit, offset i
 }
 
 var DefaultOffsetLimitPaginator = NewOffsetLimitPaginator(100)
+
+func ResponsePagination(ctx *gin.Context, count int, data interface{}) {
+	ctx.Header(HEADER_TOTAL_COUNT, utils.Num2String(count))
+	ctx.JSON(http.StatusOK, data)
+}
+
+func PaginationQuery(db *gorm.DB, t interface{}, limit, offset int) (int, error) {
+	var count int
+	count = 0
+	err := db.Count(&count).Error
+
+	if err != nil {
+		return count, err
+	}
+
+	err = db.Limit(limit).Offset(offset).Find(t).Error
+
+	if err != nil {
+		return count, err
+	}
+
+	return count, nil
+}
--- gin/ginhelper/rest.go
+++ gin/ginhelper/rest.go
@@ -3,6 +3,7 @@ package ginhelper
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
 	"net/http"
 )
 
@@ -16,65 +17,132 @@ const (
 
 var ReadOnly = []uint{List, Retrieve}
 
+type IRestView interface {
+	GetQuerySet() *gorm.DB
+	GetSerializers() interface{}
+	GetSerializer() interface{}
+	GetOrderBy() string
+	LookupField() string
+}
+
 type IRest interface {
-	List(ctx *gin.Context)
-	Create(ctx *gin.Context)
-	Retrieve(ctx *gin.Context, id string)
-	Update(ctx *gin.Context, id string)
-	Destroy(ctx *gin.Context, id string)
+	List(ctx *gin.Context, restView IRestView)
+	Create(ctx *gin.Context, restView IRestView)
+	Retrieve(ctx *gin.Context, restView IRestView, id string)
+	Update(ctx *gin.Context, restView IRestView, id string)
+	Destroy(ctx *gin.Context, restView IRestView, id string)
 }
 
 type Rest struct{}
 
-func (r *Rest) List(ctx *gin.Context) {
+func (r *Rest) List(ctx *gin.Context, restView IRestView) {
+	limit, offset := DefaultOffsetLimitPaginator.ParsePagination(ctx)
+	q := restView.GetQuerySet().Order(restView.GetOrderBy())
+
+	data := restView.GetSerializers()
+
+	count, err := PaginationQuery(q, data, limit, offset)
+	if err != nil {
+		ctx.Status(http.StatusInternalServerError)
+		return
+	}
+
+	ResponsePagination(ctx, count, data)
+}
+
+func (r *Rest) Create(ctx *gin.Context, restView IRestView) {
 	ctx.Status(http.StatusMethodNotAllowed)
 }
 
-func (r *Rest) Create(ctx *gin.Context) {
+func (r *Rest) Retrieve(ctx *gin.Context, restView IRestView, id string) {
+	data := restView.GetSerializer()
+
+	err := restView.GetQuerySet().First(data, fmt.Sprintf("%s = ?", restView.LookupField()), id).Error
+
+	if err != nil {
+		if gorm.IsRecordNotFoundError(err) {
+			ctx.Status(http.StatusNotFound)
+			return
+		}
+		ctx.Status(http.StatusInternalServerError)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, data)
+}
+
+func (r *Rest) Update(ctx *gin.Context, restView IRestView, id string) {
 	ctx.Status(http.StatusMethodNotAllowed)
 }
 
-func (r *Rest) Retrieve(ctx *gin.Context, id string) {
-	ctx.Status(http.StatusMethodNotAllowed)
+func (r *Rest) Destroy(ctx *gin.Context, restView IRestView, id string) {
+	err := restView.GetQuerySet().Delete(restView.GetSerializer(), fmt.Sprintf("%s = ?", restView.LookupField()), id).Error
+	if err != nil {
+		if gorm.IsRecordNotFoundError(err) {
+			ctx.Status(http.StatusNoContent)
+			return
+		}
+		ctx.Status(http.StatusInternalServerError)
+		return
+	}
+	ctx.Status(http.StatusNoContent)
 }
 
-func (r *Rest) Update(ctx *gin.Context, id string) {
-	ctx.Status(http.StatusMethodNotAllowed)
+type RestView struct {
 }
 
-func (r *Rest) Destroy(ctx *gin.Context, id string) {
-	ctx.Status(http.StatusMethodNotAllowed)
+func (r *RestView) GetQuerySet() *gorm.DB {
+	panic("not implement")
 }
 
-func BindRouter(r gin.IRoutes, prefix string, rest IRest, methods ...uint) {
+func (r *RestView) GetSerializers() interface{} {
+	panic("not implement")
+}
+
+func (r *RestView) GetSerializer() interface{} {
+	panic("not implement")
+}
+
+func (r *RestView) GetOrderBy() string {
+	return "created_at DESC"
+}
+
+func (r *RestView) LookupField() string {
+	return "id"
+}
+
+func BindRouter(r gin.IRoutes, prefix string, restView IRestView, rest IRest, methods ...uint) {
 	withID := fmt.Sprintf("%s/:id", prefix)
+
 	if len(methods) == 0 {
 		// list
-		r.GET(prefix, func(c *gin.Context) {
-			rest.List(c)
+		r.GET(prefix, func(ctx *gin.Context) {
+			rest.List(ctx, restView)
 		})
 
 		// post
-		r.POST(prefix, func(c *gin.Context) {
-			rest.Create(c)
+		r.POST(prefix, func(ctx *gin.Context) {
+			ctx.Status(http.StatusMethodNotAllowed)
 		})
 
 		// get one
-		r.GET(withID, func(c *gin.Context) {
-			id := c.Param("id")
-			rest.Retrieve(c, id)
+		r.GET(withID, func(ctx *gin.Context) {
+			id := ctx.Param("id")
+
+			rest.Retrieve(ctx, restView, id)
 		})
 
 		// delete one
-		r.DELETE(withID, func(c *gin.Context) {
-			id := c.Param("id")
-			rest.Destroy(c, id)
+		r.DELETE(withID, func(ctx *gin.Context) {
+			id := ctx.Param("id")
+
+			rest.Destroy(ctx, restView, id)
 		})
 
 		// update one
-		r.PATCH(withID, func(c *gin.Context) {
-			id := c.Param("id")
-			rest.Update(c, id)
+		r.PATCH(withID, func(ctx *gin.Context) {
+			//id := c.Param("id")
+			ctx.Status(http.StatusMethodNotAllowed)
 		})
 
 		return
@@ -84,31 +152,33 @@ func BindRouter(r gin.IRoutes, prefix string, rest IRest, methods ...uint) {
 		switch v {
 		case List:
 			// list
-			r.GET(prefix, func(c *gin.Context) {
-				rest.List(c)
+			r.GET(prefix, func(ctx *gin.Context) {
+				rest.List(ctx, restView)
 			})
 		case Retrieve:
 			// get one
-			r.GET(withID, func(c *gin.Context) {
-				id := c.Param("id")
-				rest.Retrieve(c, id)
+			r.GET(withID, func(ctx *gin.Context) {
+				id := ctx.Param("id")
+
+				rest.Retrieve(ctx, restView, id)
 			})
 		case Create:
 			// post
-			r.POST(prefix, func(c *gin.Context) {
-				rest.Create(c)
+			r.POST(prefix, func(ctx *gin.Context) {
+				ctx.Status(http.StatusMethodNotAllowed)
 			})
 		case Update:
 			// update one
-			r.PATCH(withID, func(c *gin.Context) {
-				id := c.Param("id")
-				rest.Update(c, id)
+			r.PATCH(withID, func(ctx *gin.Context) {
+				//id := c.Param("id")
+				ctx.Status(http.StatusMethodNotAllowed)
 			})
 		case Destroy:
 			// delete one
-			r.DELETE(withID, func(c *gin.Context) {
-				id := c.Param("id")
-				rest.Destroy(c, id)
+			r.DELETE(withID, func(ctx *gin.Context) {
+				id := ctx.Param("id")
+
+				rest.Destroy(ctx, restView, id)
 			})
 		}
 	}
