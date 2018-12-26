2507bc5d2793c0b2ff85e358c346e2f9c811db76
zcong1993 <1005997919@qq.com>
2018-12-27 01:37:33 +0800

    update docs

--- doc.go
+++ doc.go
@@ -2,3 +2,12 @@
 Package libgo is some helper functions can be reuse.
 */
 package libgo // import "github.com/zcong1993/libgo"
+
+import (
+	_ "github.com/zcong1993/libgo/gin/ginerr"
+	_ "github.com/zcong1993/libgo/gin/ginhelper"
+	_ "github.com/zcong1993/libgo/mysql"
+	_ "github.com/zcong1993/libgo/testhelper"
+	_ "github.com/zcong1993/libgo/utils"
+	_ "github.com/zcong1993/libgo/validator"
+)
