a8cd918fb04276260119c14b92af658b9f21ae2d
zcong1993 <1005997919@qq.com>
2019-01-08 02:28:11 +0800

    update pg model

--- pg/model.go
+++ pg/model.go
@@ -1,14 +1,13 @@
 package pg
 
 import (
-	"github.com/google/uuid"
 	"time"
 )
 
 // Model is model for pg with uuid as primary key
 // CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 type Model struct {
-	ID        uuid.UUID `gorm:"primary_key;type:uuid;default:uuid_generate_v4()"`
+	ID        string `gorm:"primary_key;type:uuid;default:uuid_generate_v4()"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt *time.Time `sql:"index"`
